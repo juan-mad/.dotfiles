89ecdb9d99b0c2a8065c832f6ea23a03bf5777e8
juan-mad <juanmarcosar@protonmail.com>
2024-02-28 22:42:04 +0100

    Change some plugins and remove LSP for now

--- nvim/lua/jmad/lazy.lua
+++ nvim/lua/jmad/lazy.lua
@@ -1,3 +1,6 @@
+
+-- Install the lazy.nvim plugin manager
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -11,15 +14,20 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Configure plugins
 require("lazy").setup({
+    checker = {enabled = false},
+    -- Fuzzy finder FTW
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.4',
         -- or                              , branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    -- tokyonight color themes
     {
-        'Mofiqul/vscode.nvim'
+        'folke/tokyonight.nvim'
     },
+    -- Highlight, edit, and navigate code
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
@@ -30,27 +38,36 @@ require("lazy").setup({
     {
         'mbbill/undotree'
     },
+    -- Git related plugins
     {
         'tpope/vim-fugitive'
     },
     {
-        'terrortylor/nvim-comment'
+        'tpope/vim-rhubarb'
     },
     {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        dependencies = {
-              -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {'williamboman/mason.nvim'},           -- Optional
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-        }
+        'terrortylor/nvim-comment'
     },
+    -- {
+    --     'VonHeikemen/lsp-zero.nvim',
+    --     branch = 'v2.x',
+    --     dependencies = {
+    --           -- LSP Support
+    --         {'neovim/nvim-lspconfig'},             -- Required
+    --         {'williamboman/mason.nvim'},           -- Optional
+    --         {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    --         -- Autocompletion
+    --         {'hrsh7th/nvim-cmp'},     -- Required
+    --         {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    --         {'L3MON4D3/LuaSnip'},     -- Required
+    --     }
+    -- },
+    -- Unintrusive UI for neovim notifications and LSP progress messages
+    {
+        'j-hui/fidget.nvim'
+    },
+    -- Snippets
     {
         'L3MON4D3/LuaSnip'
     },
@@ -58,7 +75,17 @@ require("lazy").setup({
         'nvim-lualine/lualine.nvim'
     },
     {
+        -- Indentation guides. See :help ibl
+        'lukas-reineke/indent-blankline.nvim',
+    },
+    {
+        'numToStr/telescope.nvim', opts = {}
+    },
+    {
         'ThePrimeagen/vim-be-good'
+    },
+    {
+        'folke/zen-mode.nvim'
     }
 })
 
